0f2d8848d0ac20e560a226af4734da5e8ecbe4b2
Dominik <niec27501@student.pwsztar.edu.pl>
2017-05-28 15:58:25 +0200

    Add unit tests and update Readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+Liczby doskonałe
+
+Funkcja getMessage(numbers) jako argument przyjmuje tablicę liczb naturalnych. 
+Korzysta ona z funkcji isPerfectNumber(number), która sprawdza czy przesłany do niej argument jest liczbą doskonałą.
+
+Funkcja getMessage(numbers) zwraca obiekt, zawierający informacje:
+ 
+    - czy w argumencie wystąpiły jakieś liczby doskonałe (has_Perfect_Numbers), 
+    - największą z nich (biggest), 
+    - odpowiednią wiadomość tekstową (message).
+   
+W przypadku błędnych argumentów, funkcja getMesssage(numbers) rzuca wyjątek.
+
+
+
+Autorzy:
+Nieć Dominik, Szczudrawa Kamil, Szul Dawid.
---
+++ test/app.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var expect = require('chai').expect;
+var app = require('../app/app');
+
+
+describe('getMessage', function () {
+    describe('When parameter is ok', function () {
+        describe('and contains perfect number(s)', function () {
+            it('should return object which contains biggest perfect number and a message', function () {
+
+                expect(app.getMessage([1, 2, 3, 4, 5, 6])).to.eql({has_Perfect_Numbers: true, biggest: 6,
+                    message: 'Argument contains 1 perfect number(s) and the biggest one is 6'});
+
+                expect(app.getMessage([100, 6, 496, 81])).to.eql({has_Perfect_Numbers: true, biggest: 496,
+                    message: 'Argument contains 2 perfect number(s) and the biggest one is 496'});
+
+            })
+        })
+
+        describe('and doesn\'t contain perfect numbers', function () {
+            it('should return object which contains info about not finding perfect numbers', function () {
+
+                expect(app.getMessage([1, 2, 3, 4, 5])).to.eql({has_Perfect_Numbers: false, biggest: null,
+                    message: 'Argument doesn\'t contain perfect numbers'});
+
+                expect(app.getMessage([100, 200, 300, 400, 888])).to.eql({has_Perfect_Numbers: false, biggest: null,
+                    message: 'Argument doesn\'t contain perfect numbers'});
+
+            })
+        })
+    })
+
+    describe('When parameter is not ok', function () {
+        it('should throw Error(\'Argument is wrong or not a number!\')', function(){
+
+            expect(function(){app.getMessage([-1, 0, -10, -496])}).to.throw('Argument is wrong or not a number!');
+            expect(function(){app.getMessage([6, '496'])}).to.throw('Argument is wrong or not a number!');
+            expect(function(){app.getMessage([5, 10, 15, ''])}).to.throw('Argument is wrong or not a number!');
+
+        });
+    })
+})
